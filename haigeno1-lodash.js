b7a4cea864341fbf6225e3e8da97262d2ba4052b
haigeno1 <2276765922@qq.com>
2021-03-03 19:27:10 +0800

    add

--- haigeno1-lodash.js
+++ haigeno1-lodash.js
@@ -2177,6 +2177,18 @@ var haigeno1 = (function(){
   }
 
 
+  // const curry = (fn, ...args1) => (...args2) => (
+  //   arg => arg.length === fn.length ? fn(...arg) : curry(fn, ...arg)
+  //  )([...args1, ...args2]);
+   
+   // 调用
+  //  const foo = (a, b, c) => a * b * c;
+  //  curry(foo)(2, 3, 4); // -> 24
+  //  curry(foo, 2)(3, 4); // -> 24
+  //  curry(foo, 2, 3)(4); // -> 24
+  //  curry(foo, 2, 3, 4)(); // -> 24
+   
+
   function debounce(f,duration){
     var id
     return function(...args){
