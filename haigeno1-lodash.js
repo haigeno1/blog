8cc2a7333175534eb18701de5ac85cd3efa95b9c
haigeno1 <2276765922@qq.com>
2021-03-11 14:57:14 +0800

    add、

--- haigeno1-lodash.js
+++ haigeno1-lodash.js
@@ -907,6 +907,31 @@ var haigeno1 = (function(){
     return res
   }
 
+  function deepClone(data) {
+    var t = type(data), o, i, ni;
+    
+    if(t === 'array') {
+      o = [];
+    }else if( t === 'object') {
+      o = {};
+    }else {
+      return data;
+    }
+    
+    if(t === 'array') {
+      for (i = 0, ni = data.length; i < ni; i++) {
+        o.push(deepClone(data[i]));
+      }
+      return o;
+    }else if( t === 'object') {
+      for( i in data) {
+        o[i] = deepClone(data[i]);
+      }
+      return o;
+    }
+  }
+
+
   function map(collection,iter=identity){
     iter = iteratee(iter)
     return collection.reduce((result,item,index,collection) => {
