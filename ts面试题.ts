e6ca810f648f8c56c5f63305d32a04ed3068bbbe
haigeno1 <2276765922@qq.com>
2022-01-28 17:11:18 +0800

    add ts

--- ts面试题.ts
+++ ts面试题.ts
@@ -1,3 +1,120 @@
+// https://github.com/microsoft/TypeScript/blob/8da3eff7b0dbb68c17a950c006edf143456b28cc/src/lib/es5.d.ts#L1442
+
+declare type PromiseConstructorLike = new <T>(executor: (resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void) => PromiseLike<T>;
+
+interface PromiseLike<T> {
+    /**
+     * Attaches callbacks for the resolution and/or rejection of the Promise.
+     * @param onfulfilled The callback to execute when the Promise is resolved.
+     * @param onrejected The callback to execute when the Promise is rejected.
+     * @returns A Promise for the completion of which ever callback is executed.
+     */
+    then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): PromiseLike<TResult1 | TResult2>;
+}
+
+/**
+ * Represents the completion of an asynchronous operation
+ */
+interface Promise<T> {
+    /**
+     * Attaches callbacks for the resolution and/or rejection of the Promise.
+     * @param onfulfilled The callback to execute when the Promise is resolved.
+     * @param onrejected The callback to execute when the Promise is rejected.
+     * @returns A Promise for the completion of which ever callback is executed.
+     */
+    then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): Promise<TResult1 | TResult2>;
+
+    /**
+     * Attaches a callback for only the rejection of the Promise.
+     * @param onrejected The callback to execute when the Promise is rejected.
+     * @returns A Promise for the completion of the callback.
+     */
+    catch<TResult = never>(onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null): Promise<T | TResult>;
+}
+
+interface ArrayLike<T> {
+    readonly length: number;
+    readonly [n: number]: T;
+}
+
+/**
+ * Make all properties in T optional
+ */
+ type Partial<T> = {
+  [P in keyof T]?: T[P];
+};
+
+/**
+* Make all properties in T required
+*/
+type Required<T> = {
+  [P in keyof T]-?: T[P];
+};
+
+/**
+* Make all properties in T readonly
+*/
+type Readonly<T> = {
+  readonly [P in keyof T]: T[P];
+};
+
+/**
+* From T, pick a set of properties whose keys are in the union K
+*/
+type Pick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+
+/**
+* Construct a type with a set of properties K of type T
+*/
+type Record<K extends keyof any, T> = {
+  [P in K]: T;
+};
+
+/**
+* Exclude from T those types that are assignable to U
+*/
+type Exclude<T, U> = T extends U ? never : T;
+
+/**
+* Extract from T those types that are assignable to U
+*/
+type Extract<T, U> = T extends U ? T : never;
+
+/**
+* Construct a type with the properties of T except for those in type K.
+*/
+type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
+
+/**
+ * Exclude null and undefined from T
+ */
+ type NonNullable<T> = T extends null | undefined ? never : T;
+
+ /**
+  * Obtain the parameters of a function type in a tuple
+  */
+ type Parameters<T extends (...args: any) => any> = T extends (...args: infer P) => any ? P : never;
+ 
+ /**
+  * Obtain the parameters of a constructor function type in a tuple
+  */
+ type ConstructorParameters<T extends new (...args: any) => any> = T extends new (...args: infer P) => any ? P : never;
+ 
+ /**
+  * Obtain the return type of a function type
+  */
+ type ReturnType<T extends (...args: any) => any> = T extends (...args: any) => infer R ? R : any;
+ 
+ /**
+  * Obtain the return type of a constructor function type
+  */
+ type InstanceType<T extends new (...args: any) => any> = T extends new (...args: any) => infer R ? R : any;
+ 
+
+
+
 
 // https://juejin.cn/post/6974713100826050591
 // 实现 FuncReturnType 工具泛型，接受一个函数类型，并且推断出函数的返回值。
@@ -6,6 +123,22 @@ type FuncReturnType<T> = T extends (...args: any[]) => infer R ? R :never;
 type fn = (name: string)=> boolean;
 type T1 = FuncReturnType<fn>;
 
+
+// 如果T包含的类型 是 U包含的类型的 '子集', 当T为联合类型的时候，会进行拆分
+type Diff<T, U> = T extends U ? never : T; // 找出T的差集
+type Filter<T, U> = T extends U ? T : never; // 找出交集
+
+type T30 = Diff<"a" | "b" | "c" | "d", "a" | "c" | "f">;  // => "b" | "d"
+// <"a" | "b" | "c" | "d", "a" | "c" | "f">
+// 相当于
+// <'a', "a" | "c" | "f"> |
+// <'b', "a" | "c" | "f"> |
+// <'c', "a" | "c" | "f"> |
+// <'d', "a" | "c" | "f">
+type T31 = Filter<"a" | "b" | "c" | "d", "a" | "c" | "f">;  // => "a" | "c"
+// <"a" | "b" | "c" | "d", "a" | "c" | "f"> 同上
+
+let demo1: Diff<number, string>; // => number
 
 
 
