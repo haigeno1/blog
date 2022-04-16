c0f26788d36ac8e3e3bc4306c71bf4e76d121161
haigeno1 <2276765922@qq.com>
2022-04-16 15:47:59 +0800

    add hascycle

--- 手写原生js/函数相关.js
+++ 手写原生js/函数相关.js
@@ -210,8 +210,8 @@ function compose() {
 
 const compose =
   (...fns) =>
-  (arg) =>
-    fns.reduce((composed, f) => f(compose), arg);
+    (arg) =>
+      fns.reduce((composed, f) => f(compose), arg);
 
 const compose = (...fns) =>
   fns.reduce(
@@ -633,20 +633,23 @@ function hasLoop(obj) {
   // 判断对象内部是否有和源相同的属性
   function findLoop(target, src) {
     // 源数组，并将自身传入
+    // 添加的操作在这里
     const source = src.slice().concat([target]);
 
     for (const key in target) {
       // 如果是对象才需要判断
       if (typeof target[key] === "object") {
-        // 如果在源数组中找到 || 递归查找内部属性找到相同
+        // 如果在源数组中找到
         const res1 = source.indexOf(target[key]) > -1;
         if (res1) {
           return true;
         }
+        // 递归查找内部属性找到相同
         const res2 = findLoop(target[key], source);
         if (res2) {
           return true;
         }
+        // 合并简写
         // if (source.indexOf(target[key]) > -1 || findLoop(target[key], source)) {
         //     return true
         // }
@@ -657,6 +660,30 @@ function hasLoop(obj) {
   // 如果传入值是对象，则执行判断，否则返回false
   return typeof obj === "object" ? findLoop(obj, []) : false;
 }
+
+
+function cycleDetector(obj, objAry) {
+  var objAry = objAry || [obj];
+  var flag = false;
+  for (var name in obj) {
+    if (typeof obj[name] == "object" && obj[name] != null) {
+      if (objAry.indexOf(obj[name]) != -1) {
+        flag = true;
+        break;
+      } else {
+        // 先push 后pop  添加的操作在这里
+        objAry.push(obj[name]);
+        flag = cycleDetector(obj[name], objAry);
+        if (flag == true) {
+          break;
+        }
+        objAry.pop();
+      }
+    }
+  }
+  return flag;
+}
+
 
 let tmp = { age: 18 };
 let src = {
