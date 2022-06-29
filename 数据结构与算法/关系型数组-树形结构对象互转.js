9bf6795d68b397ff1b7c8dc991b1883f804ae522
haigeno1 <2276765922@qq.com>
2022-06-29 13:45:13 +0800

    arrToTree

--- 数据结构与算法/关系型数组-树形结构对象互转.js
+++ 数据结构与算法/关系型数组-树形结构对象互转.js
@@ -1,39 +1,90 @@
 
 
 // 3. 关系型数组转换成树形结构对象
+// 递归 时间换空间 时间复杂度高空间复杂度低
 function arrToTree(arr, pid = null) {
   let result = []
   arr.forEach(item => {
-      if (item.parent === pid) {
-          const children = arrToTree(arr, item.id)
-          if (children.length > 0) {
-              result.push({ ...item, children })
-          } else {
-              result.push({ ...item })
-          }
+    if (item.parent === pid) {
+      const children = arrToTree(arr, item.id)
+      if (children.length > 0) {
+        result.push({ ...item, children })
+      } else {
+        result.push({ ...item })
       }
+    }
   })
   return result
 }
 var obj = [
-  { "id": 3, "parent": 2 },
   { "id": 1, "parent": null },
-  { "id": 2, "parent": 1 }
+  { "id": 2, "parent": 1 },
+  { "id": 3, "parent": 2 },
+  { "id": 4, "parent": 1 },
 ]
 arrToTree(obj)
+
+// [
+//   {
+//       "id": 1,
+//       "parent": null,
+//       "children": [
+//           {
+//               "id": 2,
+//               "parent": 1,
+//               "children": [
+//                   {
+//                       "id": 3,
+//                       "parent": 2
+//                   }
+//               ]
+//           },
+//           {
+//               "id": 4,
+//               "parent": 1
+//           }
+//       ]
+//   }
+// ]
+
 // 这里最外层是数组而不是对象 如果需要时对象就处理下
-// o = {
-//     "obj": {
-//         "id": 1,
-//         "parent": null,
-//         "children": [{
-//             "id": 2,
-//             "parent": 1,
-//             "children": [{
-//                 "id": 3,
-//                 "parent": 2
-//             }]
-//         }]
-//     }N
-// }
+
+
+// 一次遍历 用itemMap额外空间换取时间
+// 链接：https://juejin.cn/post/6983904373508145189
+function arrayToTree(items) {
+  const result = [];   // 存放结果集
+  const itemMap = {};  // 
+  for (const item of items) {
+    const id = item.id;
+    const pid = item.pid;
+
+    if (!itemMap[id]) {
+      itemMap[id] = {
+        children: [],
+      }
+    }
+
+    itemMap[id] = {
+      ...item,
+      children: itemMap[id]['children']
+    }
+
+    const treeItem = itemMap[id];
+
+    if (pid === 0) {
+      result.push(treeItem);
+    } else {
+      if (!itemMap[pid]) {
+        itemMap[pid] = {
+          children: [],
+        }
+      }
+      itemMap[pid].children.push(treeItem)
+    }
+
+  }
+  return result;
+}
+
 
